d6a15b3bdeb70ad443d6616373acd938ec63dfac
luosuolaotaipo <luosuolaotaipo.xu@gmail.com>
2017-12-05 22:39:50 +0800

    add node(DOM)  operation

--- gallery-web-design-with-js-DOM/gallery-script.js
+++ gallery-web-design-with-js-DOM/gallery-script.js
@@ -1,4 +1,14 @@
 function showPic(whichpic){
     var srcpic = document.getElementById("placeholder");
     srcpic.setAttribute("src",whichpic.getAttribute("href"));
+    var desctext=whichpic.getAttribute("title");
+    var descrption=document.getElementById("discription");
+    descrption.childNodes[0].nodeValue=desctext;
 }
+
+function countBodyChildren(){
+    var body_children=document.getElementsByTagName("body")[0];
+    alert(body_children.nodeType);
+}
+// window.onload=countBodyChildren();
+

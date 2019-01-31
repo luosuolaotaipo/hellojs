b10ef4e8805e5cd4c4f5abbff0694d4600b8e987
Huimin <huimin.xu@accenture.com>
2019-01-31 13:37:44 +0800

    add webpack

---
+++ learn-webpack/src/index.js
@@ -0,0 +1,13 @@
+import _ from 'lodash';
+import './mycss.css';
+function component() {
+    console.log('hello world');
+    var element = document.createElement('div');
+  
+    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    return element;
+  }
+  
+document.body.appendChild(component());
